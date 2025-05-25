44799d3185a2e2bf3569b470cb0bf4a362fb5b0e
Spencerdsheel <spencerdsheel487@gmail.com>
2025-05-25 23:44:29 +0530

    User integration

--- interactive_dashboard/settings.py
+++ interactive_dashboard/settings.py
@@ -102,8 +102,12 @@ DATABASES = {
     },
 
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('MAIN_NAME'),
+        'USER': os.getenv('MAIN_USER'),
+        'PASSWORD': os.getenv('MAIN_PASSWORD'),
+        'HOST': os.getenv('MAIN_HOST'),
+        'PORT': os.getenv('MAIN_PORT'),
     }
 }
 
